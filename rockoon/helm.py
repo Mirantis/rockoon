95850a2d47b0aa7308201b0f19cd767ec92a792a
Vasyl Saienko <vsaienko@mirantis.com>
2025-10-24 14:46:43 +0300

    Get rid of async in helm
    Related-Prod: PRODX-3456
    
    Change-Id: I338fb886538e690772717834bb948b4b81467f37

--- rockoon/helm.py
+++ rockoon/helm.py
@@ -1,15 +1,13 @@
-import asyncio
-import contextlib
-from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime
 import os
 import re
 import yaml
 import tempfile
-import threading
-from asyncio.subprocess import PIPE
+import subprocess
+from subprocess import PIPE
 
 import kopf
+from filelock import FileLock
 
 from rockoon import utils
 from rockoon import constants
@@ -19,36 +17,17 @@ from rockoon import settings
 
 LOG = utils.get_logger(__name__)
 
-HELM_LOCK = threading.Lock()
 CONF = settings.CONF
-
-_pool = ThreadPoolExecutor()
-
-
-@contextlib.asynccontextmanager
-async def helm_lock(lock, cmd):
-    LOG.debug(f"Acquiring helm lock {lock} for cmd {cmd}")
-    loop = asyncio.get_event_loop()
-    await loop.run_in_executor(_pool, lock.acquire)
-    LOG.debug(f"Acquired helm lock {lock} for cmd {cmd}")
-    try:
-        yield  # the lock is held
-    finally:
-        lock.release()
-        LOG.debug(f"Helm lock {lock} for cmd {cmd} is released")
 
 
 def helm_retry(func):
-    async def wrapper(*args, **kwargs):
+    def wrapper(*args, **kwargs):
         attempt = 1
         max_retries = CONF.getint("helmbundle", "helm_max_retries")
         while attempt <= max_retries:
             try:
                 start = datetime.utcnow()
-                res = await asyncio.wait_for(
-                    func(*args, **kwargs),
-                    timeout=CONF.getint("helmbundle", "helm_cmd_timeout"),
-                )
+                res = func(*args, **kwargs)
                 running_for = (datetime.utcnow() - start).total_seconds()
                 LOG.info(
                     f"Running helm {args}, {kwargs} command took {running_for}"
@@ -58,7 +37,7 @@ def helm_retry(func):
             except (
                 exception.HelmImmutableFieldChange,
                 exception.HelmRollback,
-                asyncio.exceptions.TimeoutError,
+                subprocess.TimeoutExpired,
             ) as e:
                 LOG.warning(
                     f"Got retriable exception {e}, when calling {func}, retrying, attempt: {attempt}"
@@ -89,6 +68,7 @@ class HelmManager:
             }
         )
         self.env = os_env
+        self.lock = FileLock("/tmp/rockoon_helm.lock")
 
     def _substitute_local_proxy(self, repo):
         node_ip = os.environ["NODE_IP"]
@@ -102,7 +82,7 @@ class HelmManager:
         ][0]
         return os.path.join(settings.HELM_CHARTS_DIR, chart_group, chart)
 
-    async def _guess_and_delete(self, stderr):
+    def _guess_and_delete(self, stderr):
         immutable_pattern = r'cannot patch "(.*?)" with kind ([a-zA-Z]+):'
         for match in re.findall(immutable_pattern, stderr):
             try:
@@ -128,46 +108,46 @@ class HelmManager:
                 kube.wait_for_deleted(obj)
             LOG.info(f"Successfully removed kind: {kind} with name {name}")
 
-    async def _rollback(self, name, args=None):
+    def _rollback(self, name, args=None):
         args = args or []
+        timeout = CONF.getint("helmbundle", "helm_cmd_timeout")
+
         LOG.info(f"Rolling back release {name}")
         cmd = [
+            self.binary,
             "rollback",
             name,
             "--namespace",
             self.namespace,
             *args,
         ]
-        process = await asyncio.create_subprocess_exec(
-            self.binary,
-            *cmd,
+        res = subprocess.run(
+            cmd,
             env=self.env,
             stdin=PIPE,
             stdout=PIPE,
             stderr=PIPE,
+            timeout=timeout,
         )
-        stdout, stderr = await process.communicate()
-        stdout = stdout.decode() or None
-        stderr = stderr.decode() or None
+        stdout, stderr = res.stdout.decode(), res.stderr.decode()
         return (stdout, stderr)
 
     @helm_retry
-    async def _run_cmd(self, cmd, raise_on_error=True, release_name=None):
+    def _run_cmd(self, cmd, raise_on_error=True, release_name=None):
         LOG.info(
             "Running helm command started: '%s'",
             cmd,
         )
-        process = await asyncio.create_subprocess_exec(
-            self.binary,
-            *cmd,
+        timeout = CONF.getint("helmbundle", "helm_cmd_timeout")
+        res = subprocess.run(
+            [self.binary, *cmd],
             env=self.env,
             stdin=PIPE,
             stdout=PIPE,
             stderr=PIPE,
+            timeout=timeout,
         )
-        stdout, stderr = await process.communicate()
-        stdout = stdout.decode()
-        stderr = stderr.decode()
+        stdout, stderr = res.stdout.decode(), res.stderr.decode()
 
         LOG.debug(
             "Helm command %s output is: stdout: %s, stderr: %s",
@@ -175,7 +155,7 @@ class HelmManager:
             stdout,
             stderr,
         )
-        if process.returncode and raise_on_error:
+        if res.returncode and raise_on_error:
             LOG.error(
                 "Helm command failed. stdout: %s, stderr: %s",
                 stdout,
@@ -185,7 +165,7 @@ class HelmManager:
                 ("field is immutable", "are forbidden")
             ):
                 LOG.warning("Trying to modify object")
-                await self._guess_and_delete(stderr)
+                self._guess_and_delete(stderr)
                 raise exception.HelmImmutableFieldChange()
             if (
                 "another operation (install/upgrade/rollback) is in progress"
@@ -195,19 +175,18 @@ class HelmManager:
                 LOG.warning(
                     f"The release {release_name} stuck in install/upgrade/rollback. Rollback it."
                 )
-                await asyncio.wait_for(
-                    self._rollback(release_name),
-                    timeout=CONF.getint("helmbundle", "helm_cmd_timeout"),
-                )
+                self._rollback(release_name)
                 raise exception.HelmRollback()
             raise kopf.TemporaryError("Helm command failed")
         return (stdout, stderr)
 
-    async def run_cmd(self, cmd, raise_on_error=True, release_name=None):
-        async with helm_lock(HELM_LOCK, cmd):
-            return await self._run_cmd(cmd, raise_on_error, release_name)
+    def run_cmd(self, cmd, raise_on_error=True, release_name=None):
+        LOG.debug(f"Acquiring helm lock for cmd {cmd}")
+        with self.lock:
+            LOG.debug(f"Acquired helm lock for cmd {cmd}")
+            return self._run_cmd(cmd, raise_on_error, release_name)
 
-    async def exist(self, name, args=None):
+    def exist(self, name, args=None):
         args = args or []
         cmd = [
             "list",
@@ -217,12 +196,12 @@ class HelmManager:
             "json",
             *args,
         ]
-        stdout, stderr = await self.run_cmd(cmd)
+        stdout, stderr = self.run_cmd(cmd)
         for release in yaml.safe_load(stdout):
             if release["name"] == name:
                 return True
 
-    async def list(self, args=None):
+    def list(self, args=None):
         args = args or []
         cmd = [
             "list",
@@ -232,10 +211,10 @@ class HelmManager:
             "json",
             *args,
         ]
-        stdout, stderr = await self.run_cmd(cmd)
+        stdout, stderr = self.run_cmd(cmd)
         return yaml.safe_load(stdout)
 
-    async def get_release_values(self, name, args=None):
+    def get_release_values(self, name, args=None):
         args = args or []
         cmd = [
             "get",
@@ -247,10 +226,10 @@ class HelmManager:
             "json",
             *args,
         ]
-        stdout, stderr = await self.run_cmd(cmd)
+        stdout, stderr = self.run_cmd(cmd)
         return yaml.safe_load(stdout)
 
-    async def set_release_values(self, name, values, chart, args=None):
+    def set_release_values(self, name, values, chart, args=None):
         args = args or []
         chart_url = self.get_chart_url(chart)
         # Avoid using --reuse-values, it drops values for overrides related upstream
@@ -258,7 +237,7 @@ class HelmManager:
         with tempfile.NamedTemporaryFile(
             mode="w", prefix=name, delete=True
         ) as tmp:
-            current_values = await self.get_release_values(name)
+            current_values = self.get_release_values(name)
             utils.merger.merge(current_values, values)
             yaml.dump(current_values, tmp)
             cmd = [
@@ -273,9 +252,9 @@ class HelmManager:
                 self.max_history,
                 *args,
             ]
-            return await self.run_cmd(cmd, release_name=name)
+            return self.run_cmd(cmd, release_name=name)
 
-    async def install(self, name, values, chart, args=None):
+    def install(self, name, values, chart, args=None):
         args = args or []
         chart_url = self.get_chart_url(chart)
         with tempfile.NamedTemporaryFile(
@@ -295,32 +274,32 @@ class HelmManager:
                 "--install",
                 *args,
             ]
-            return await self.run_cmd(cmd, release_name=name)
+            return self.run_cmd(cmd, release_name=name)
 
-    async def install_bundle(self, data):
+    def install_bundle(self, data):
         for release in data["spec"]["releases"]:
             chart = release["chart"]
-            await self.install(
+            self.install(
                 release["name"],
                 release["values"],
                 chart,
             )
 
-    async def delete(self, name, args=None):
+    def delete(self, name, args=None):
         args = args or []
         cmd = ["delete", name, "--namespace", self.namespace, *args]
 
-        stdout, stderr = await self.run_cmd(cmd, raise_on_error=False)
+        stdout, stderr = self.run_cmd(cmd, raise_on_error=False)
         if stderr and "Release not loaded" not in stderr:
             raise kopf.TemporaryError(f"Helm command failed: {stderr}")
 
-    async def delete_bundle(self, available_releases):
-        current_releases = [x["name"] for x in await self.list()]
+    def delete_bundle(self, available_releases):
+        current_releases = [x["name"] for x in self.list()]
         to_delete = set(current_releases).intersection(available_releases)
         for release in to_delete:
-            await self.delete(release)
+            self.delete(release)
 
-    async def delete_not_active_releases(self, data, available_releases):
+    def delete_not_active_releases(self, data, available_releases):
         """Remove releases which a dynamic and enabled by feature flag."""
 
         if not available_releases:
@@ -329,4 +308,4 @@ class HelmManager:
         to_remove = set(available_releases) - set(current_releases)
 
         for release in to_remove:
-            await self.delete(release)
+            self.delete(release)
